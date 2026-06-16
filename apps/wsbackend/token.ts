5ea96681d640e3dd19c3790088b1c4b3ce297d85
coderrahul-cyber <rahulsmant299@gmail.com>
2026-06-16 09:46:50 +0530

    Working Perfectly fine

--- apps/wsbackend/token.ts
+++ apps/wsbackend/token.ts
@@ -2,7 +2,7 @@
 import jwt from 'jsonwebtoken'
 
 const token = jwt.sign(
-  { id: 'user-test-2', name: 'Test User1' },
+  { id: 'user-test-3', name: 'Test User2' },
   'hirahulsinghsmant9012',   // ← must match JWT_SECRET in ws-server/.env
   { expiresIn: '24h' }
 )
